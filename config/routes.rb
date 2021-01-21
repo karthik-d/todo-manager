f00a361c1f9a12fcf6fc289efa6db07b05b48f01
Karthik-D <karthikdesingu2000@gmail.com>
2021-01-22 02:16:35 +0530

    Added a route that shows a static message

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "todos", to: "todos#index"
 end
+
+# <HTTP Method> <url>, to:"<class>#<method>"
+# <class> is actually ClassController. Eg: student => StudentController
